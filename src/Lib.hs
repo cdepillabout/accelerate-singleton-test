100c1b10694b26699ae21ce7f6157232c55ad018
(cdep)illabout <cdep.illabout@gmail.com>
2018-03-05 00:28:30 +0900

    Get something kind of working.

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RankNTypes #-}
@@ -23,9 +24,13 @@
 module Lib where
 
 import Data.Array.Accelerate
-import Data.Singletons.Prelude
+import Data.Singletons
 import Data.Singletons.TH
+import Data.Singletons.Prelude.Num
+import Data.Singletons.TypeLits
 import GHC.Types (Type)
+import GHC.TypeLits
+import Unsafe.Coerce (unsafeCoerce)
 
 -- $(singletons [d|
 --   data Blah = Blah
@@ -39,34 +44,40 @@ import GHC.Types (Type)
 -- Blah --
 ----------
 
-data Blah = Blan_constr
+-- data Blah = Blan_constr
 
-type BlahSym = Blan_constr
+-- data instance Sing (a :: Blah) where
+--   SBlah :: Sing 'Blan_constr
+--   -- a ~ Blan_constr => SBlah
 
-data instance Sing (a :: Blah) =
-  a ~ Blan_constr => SBlah
+-- type SBlah = (Sing :: Blah -> Type)
 
-type SBlah = (Sing :: Blah -> Type)
+-- instance SingKind Blah where
+--   type Demote Blah = Blah
 
-instance SingKind Blah where
-  type Demote Blah = Blah
+--   fromSing :: Sing (a :: Blah) -> Blah
+--   fromSing SBlah = Blan_constr
 
-  fromSing SBlah = Blan_constr
+--   toSing :: Blah -> SomeSing Blah
+--   toSing Blan_constr = SomeSing SBlah
 
-  toSing Blan_constr = SomeSing SBlah
+-- instance SingI Blan_constr where
+--   sing = SBlah
 
-instance SingI Blan_constr where
-  sing = SBlah
+-- lalaBlah :: Blah -> IO ()
+-- lalaBlah blah = withSomeSing blah $ \case
+--   SBlah -> undefined
+
 
 -------------
 -- MyMaybe --
 -------------
 
-data MyMaybe a = MyNothing | MyJust a
+-- data MyMaybe a = MyNothing | MyJust a
 
-type MyNothingSym = MyNothing
+-- type MyNothingSym = MyNothing
 
-type MyJustSym1 (a :: k) = MyJust a
+-- type MyJustSym1 (a :: k) = MyJust a
 
 -- instance SuppressUnusedWarnings MyJustSym0 where
 --   suppressUnusedWarnings _ =
@@ -74,41 +85,48 @@ type MyJustSym1 (a :: k) = MyJust a
 --         ((ghc-prim-0.5.1.1:GHC.Tuple.(,) MyJustSym0KindInference)
 --            ghc-prim-0.5.1.1:GHC.Tuple.())
 
-data MyJustSym0 (u :: TyFun k (MyMaybe k))
-  = forall a. SameKind (Apply MyJustSym0 a) (MyJustSym1 a) => MyJustSym0KindInference
+-- data MyJustSym0 (u :: TyFun k (MyMaybe k))
+--   = forall a. SameKind (Apply MyJustSym0 a) (MyJustSym1 a) => MyJustSym0KindInference
 
-type instance Apply MyJustSym0 u = MyJust u
+-- type instance Apply MyJustSym0 u = MyJust u
 
-data instance Sing (z :: MyMaybe a)
-  = z ~ MyNothing => SMyNothing
-  | forall (n :: a). z ~ MyJust n => SMyJust (Sing (n :: a))
+-- data instance Sing (z :: MyMaybe a) where
+--   SMyNothing :: Sing 'MyNothing
+--   SMyJust :: forall (n :: a). Sing (n :: a) -> Sing ('MyJust n)
 
-type SMyMaybe = (Sing :: MyMaybe a -> Type)
+-- type SMyMaybe = (Sing :: MyMaybe a -> Type)
 
-instance SingKind a => SingKind (MyMaybe a) where
-  type Demote (MyMaybe a) = MyMaybe (Demote a)
+-- instance SingKind a => SingKind (MyMaybe a) where
+--   type Demote (MyMaybe a) = MyMaybe (Demote a)
 
-  fromSing SMyNothing = MyNothing
-  fromSing (SMyJust b) = MyJust (fromSing b)
+--   fromSing :: Sing (x :: MyMaybe a) -> MyMaybe (Demote a)
+--   fromSing SMyNothing = MyNothing
+--   fromSing (SMyJust b) = MyJust (fromSing b)
 
-  toSing MyNothing = SomeSing SMyNothing
-  toSing (MyJust b)
-    = case toSing b :: SomeSing a of
-        SomeSing c -> SomeSing (SMyJust c)
+--   toSing :: MyMaybe (Demote a) -> SomeSing (MyMaybe a)
+--   toSing MyNothing = SomeSing SMyNothing
+--   toSing (MyJust b)
+--     = case toSing b :: SomeSing a of
+--         SomeSing c -> SomeSing (SMyJust c)
 
-instance SingI MyNothing where
-  sing = SMyNothing
+-- instance SingI MyNothing where
+--   sing = SMyNothing
 
-instance SingI n => SingI (MyJust (n :: a)) where
-  sing = SMyJust sing
+-- instance SingI n => SingI (MyJust (n :: a)) where
+--   sing = SMyJust sing
+
+-- lalaMyMaybe :: SingKind a => MyMaybe (Demote a) -> IO ()
+-- lalaMyMaybe myMaybe = withSomeSing myMaybe $ \case
+--   SMyNothing -> undefined
+--   SMyJust b -> undefined
 
 -------------
 -- MyTuple --
 -------------
 
-data MyTuple a b = MyTuple_constr a b
+-- data MyTuple a b = MyTuple_constr a b
 
-type MyTupleSym2 (a :: k1) (b :: k2) = MyTuple_constr a b
+-- type MyTupleSym2 (a :: k1) (b :: k2) = MyTuple_constr a b
 
 -- instance SuppressUnusedWarnings MyTupleSym1 where
 --   suppressUnusedWarnings _
@@ -182,5 +200,64 @@ type MyTupleSym2 (a :: k1) (b :: k2) = MyTuple_constr a b
 -- instance SingI (':.) :: (head :. tail)) where
 --   sing = undefined
 
+data MyNat where
+  Zero :: MyNat
+  Succ :: MyNat -> MyNat
 
+$(genSingletons [''Z, ''(Data.Array.Accelerate.:.), ''MyNat])
+
+-- type family ShapeOfType (n :: Nat) where
+--   ShapeOfType 0 = Z
+--   ShapeOfType n = ShapeOfType (n :- 1) :. Int
+
+-- class ShapeOf (n :: Nat) where
+--   shapeOf :: Proxy n -> ShapeOfType n
+
+-- instance {-# OVERLAPPING #-} ShapeOf 0 where
+--   shapeOf :: Proxy 0 -> Z
+--   shapeOf _ = Z
+
+-- instance {-# OVERLAPPABLE #-} ShapeOf n where
+--   shapeOf :: forall n. Proxy n -> ShapeOfType n
+--   shapeOf _ = unsafeCoerce (shapeOf (Proxy :: Proxy (n :- 1)) :. 1)
+
+-- shapeOf :: forall n. Proxy n -> ShapeOfType n
+-- shapeOf _ = case sing :: Sing n of
+--               SNat -> unsafeCoerce (shapeOf (Proxy :: Proxy (n :- 1)) :. 1)
+
+-- natToShape :: forall n. (KnownNat (n :- 1), KnownNat n) => SNat n -> ShapeOfType n
+-- natToShape snat =
+--   let nat = natVal snat
+--   in
+--   if nat Prelude.== 0
+--     then unsafeCoerce Z
+--     else unsafeCoerce (natToShape (SNat :: SNat (n :- 1)) :. 1)
+
+type family ShapeOfType (n :: MyNat) where
+  ShapeOfType Zero = Z
+  ShapeOfType (Succ n) = ShapeOfType n :. Int
+
+natToShape :: forall n. SMyNat n -> ShapeOfType n
+natToShape SZero = Z
+natToShape (SSucc n) = natToShape n :. 1
+
+type family NatToSMyNat (n :: Nat) where
+  NatToSMyNat 0 = 'Zero
+  NatToSMyNat n = 'Succ (NatToSMyNat (n :- 1))
+
+snatToMyNat :: KnownNat n => proxy n -> SMyNat (NatToSMyNat n)
+snatToMyNat snat =
+  let n = natVal snat
+  in case n of
+    0 -> unsafeCoerce SZero
+    n ->
+      case someNatVal (n - 1) of
+        Nothing -> error "error"
+        Just (SomeNat (Proxy :: Proxy m)) ->
+          unsafeCoerce (SSucc (snatToMyNat (Proxy :: Proxy m)))
+
+papapa :: Integer -> IO ()
+papapa int =
+  withSomeSing int $ \case
+    snat@SNat -> natToShape (snatToMyNat snat)
 
